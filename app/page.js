d21f9fc74ceb3b7d92279e97600cc114cedc8c34
freire-guido <14301656+freire-guido@users.noreply.github.com>
2024-11-09 17:06:27 -0300

    images

--- app/page.js
+++ app/page.js
@@ -10,12 +10,9 @@ export default async function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center">
         <h2>{subject['bioname']}</h2>
-        <Image
-            aria-hidden
-            src="/Walt_Minnick.jpg"
-            alt="Walt Minnick"
-            width={250}
-            height={250}
+        <img
+            className='w-3/4'
+            src={`https://theunitedstates.io/images/congress/original/${subject['bioguide_id']}.jpg`}
           />
         <div className="flex flex-row w-72 gap-2">
           <a>Dem</a>
