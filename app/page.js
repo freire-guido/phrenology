4b539db889343a45348d82a74532d7892b689684
freire-guido <14301656+freire-guido@users.noreply.github.com>
2024-11-09 20:03:34 -0300

    center rip

--- app/page.js
+++ app/page.js
@@ -3,9 +3,11 @@ import GameComponent from "@/components/gamecomponent"
 
 export default async function Home() {
   let subject = await fetch(`${process.env.FETCH_URL}/api/subject`).then(res => res.json())
+  const res = await fetch(`https://theunitedstates.io/images/congress/original/${subject['bioguide_id']}.jpg`)
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1 className="text-2xl font-bold">Phrenology</h1>
       <main className="flex flex-col gap-8 row-start-2 items-center">
         <GameComponent subject={subject} key={subject['bioguide_id']}/>
       </main>
--- components/gamecomponent.js
+++ components/gamecomponent.js
@@ -1,29 +1,37 @@
 "use client"
-import React from "react"
+import React, { useEffect } from "react"
 
 import { Slider } from "@/components/ui/slider"
 import { Button } from "@/components/ui/button"
 
 export default function({subject}) {
+    useEffect(() => {
+        async function checkImage() {
+            const res = await fetch(`https://theunitedstates.io/images/congress/original/${subject['bioguide_id']}.jpg`)
+            if (res.status != 200) {
+                window.location.reload()
+            }
+        }
+        checkImage()
+    }, [])
+
     const [locked, setLocked] = React.useState(false)
     const [slider, setSlider] = React.useState(50)
 
     const guessedDem = slider <= 50
     const isDem = subject['party'] == 'Democrat'
-    
+
     return (<>
         {locked ? <h2>{subject['bioname']} - {subject['party']}</h2> : <h2>&nbsp;</h2>}
-        <div className="relative">
-            <img
-                className='h-96'
-                src={`https://theunitedstates.io/images/congress/original/${subject['bioguide_id']}.jpg`}
-                />
+            <div className={locked && (guessedDem != isDem) ? "ripped rippable" : "rippable"} style={{position: "relative", height: "30vh"}}>
+                <img className="left-ripped" src={`https://theunitedstates.io/images/congress/original/${subject['bioguide_id']}.jpg`}/>
+                <img className="right-ripped" src={`https://theunitedstates.io/images/congress/original/${subject['bioguide_id']}.jpg`}/>
             {locked && <div className="flex flex-col place-content-center items-center absolute top-0 left-0 right-0 bottom-0 text-9xl">
                 <span className="flip">
                     {guessedDem == isDem ? '✅' : '🚫'}
                 </span>
             </div>}
-        </div>
+            </div>
         <div className="flex flex-row w-72 gap-2">
             <a>Dem</a>
             <Slider defaultValue={[50]} max={100} step={10} onValueCommit={(v) => setSlider(v)}></Slider>
