e15d6060f622a579f21e41267df6f8bbd2712330
freire-guido <14301656+freire-guido@users.noreply.github.com>
2024-11-10 15:03:10 -0300

    predeploy

--- app/api/subject/route.js
+++ app/api/subject/route.js
@@ -1,9 +1,7 @@
 import {promises as fs} from 'fs'
 
-const file = fs.readFile(process.cwd() + '/app/data/subjects.json')
-
 export async function GET(request) {
-    const searchParams = request.nextUrl.searchParams
+    const file = fs.readFile(process.cwd() + '/app/data/subjects.json')
     const subjects = JSON.parse(await file)
 
     return Response.json(subjects[Math.floor(Math.random() * subjects.length)])
