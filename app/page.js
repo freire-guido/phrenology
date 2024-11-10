758adae42693d0a824aec51b1b18dea03439916a
freire-guido <14301656+freire-guido@users.noreply.github.com>
2024-11-10 15:00:27 -0300

    predeploy

--- app/page.js
+++ app/page.js
@@ -2,13 +2,13 @@ import Image from "next/image"
 import GameComponent from "@/components/gamecomponent"
 
 export default async function Home() {
-  let subject = await fetch(`${process.env.FETCH_URL}/api/subject`).then(res => res.json())
+  // let subject = await fetch(`${process.env.FETCH_URL}/api/subject`).then(res => res.json())
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-2xl font-bold">Phrenology</h1>
       <main className="flex flex-col gap-8 row-start-2 items-center">
-        <GameComponent subject={subject} key={subject['bioguide_id']}/>
+        {/* <GameComponent subject={subject} key={subject['bioguide_id']}/> */}
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
